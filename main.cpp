dbdebfd60b34b8096eca01d66689de2cdcc3bed2
MicleaChristian <engichris32@gmail.com>
2022-03-15 14:21:32 +0100

    test push

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+int main()
+{
+
+	video::E_DRIVER_TYPE driverType;
+
+	printf("Please select the driver you want for this example:\n"\
+		" (a) OpenGL 1.5\n (b) Direct3D 9.0c\n (c) Direct3D 8.1\n"\
+		" (d) Burning's Software Renderer\n (e) Software Renderer\n"\
+		" (f) NullDevice\n (otherKey) exit\n\n");
+
+	char i;
+	std::cin >> i;
+
+	switch(i)
+	{
+		case 'a': driverType = video::EDT_OPENGL;   break;
+		case 'b': driverType = video::EDT_DIRECT3D9;break;
+		case 'c': driverType = video::EDT_DIRECT3D8;break;
+		case 'd': driverType = video::EDT_BURNINGSVIDEO;break;
+		case 'e': driverType = video::EDT_SOFTWARE; break;
+		case 'f': driverType = video::EDT_NULL;     break;
+		default: return 1;
+	}
+
+
+	IrrlichtDevice *device =
+		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(1920, 1080), 16,
+			false, false, false, 0);
+
+	if (!device)
+		return 1;
+
+	device->setWindowCaption(L"Test Modifications");
+
+
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+		rect<s32>(10,10,260,22), true);
+
+	IAnimatedMesh* mesh = smgr->getMesh("assets/ninja.b3d");
+	if (!mesh)
+	{
+		device->drop();
+		return 1;
+	}
+	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+
+	if (node)
+	{
+		node->setMaterialFlag(EMF_LIGHTING, false);
+		node->setMD2Animation(scene::EMAT_STAND);
+		node->setMaterialTexture( 0, driver->getTexture("media/nskinbl.jpg") );
+	}
+
+	smgr->addCameraSceneNode(0, vector3df(0,10,10), vector3df(0,4,0));
+
+	/*
+	Ok, now we have set up the scene, lets draw everything: We run the
+	device in a while() loop, until the device does not want to run any
+	more. This would be when the user closes the window or presses ALT+F4
+	(or whatever keycode closes a window).
+	*/
+	while(device->run())
+	{
+		/*
+		Anything can be drawn between a beginScene() and an endScene()
+		call. The beginScene() call clears the screen with a color and
+		the depth buffer, if desired. Then we let the Scene Manager and
+		the GUI Environment draw their content. With the endScene()
+		call everything is presented on the screen.
+		*/
+		driver->beginScene(true, true, SColor(255,100,101,140));
+
+		smgr->drawAll();
+		guienv->drawAll();
+
+		driver->endScene();
+	}
+
+	/*
+	After we are done with the render loop, we have to delete the Irrlicht
+	Device created before with createDevice(). In the Irrlicht Engine, you
+	have to delete all objects you created with a method or function which
+	starts with 'create'. The object is simply deleted by calling ->drop().
+	See the documentation at irr::IReferenceCounted::drop() for more
+	information.
+	*/
+	device->drop();
+
+	return 0;
+}
+
+/*
+That's it. Compile and run.
+**/
